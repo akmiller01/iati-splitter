1ecb37281d4dc45d1e59cc2b6633aad996a11983
Alex Miller <alex.k.miller@gmail.com>
2020-02-28 09:59:35 -0500

    Split by filesize

--- split.py
+++ split.py
@@ -4,45 +4,42 @@ from copy import deepcopy
 from math import floor
 
 
-def get_identifier(activity):
-    return activity.xpath("//iati-identifier")[0].text
-
-
-def split_iati(max_activities, filename):
+def split_iati(target_mb, filename, out_dir=None):
     # Parse XML
-    filename_base = os.path.splitext(filename)[0]
+
+    if out_dir is None:
+        out_dir = os.path.dirname(filename)
+    filename_base = os.path.splitext(os.path.basename(filename))[0]
     tree = etree.parse(filename)
     root = tree.getroot()
 
-    # Sort root activities
+    # Get root activities
     root_activities = root.xpath("//iati-activity")
-    root_activities[:] = sorted(root_activities, key=get_identifier)
+
+    # Calculate rough activities per file
+    length_activities = len(root_activities)
+    filesize_mb = os.stat(filename).st_size / (1024 * 1024)
+    target_ratio = min(target_mb / filesize_mb, 1)
+    max_activities = round(length_activities * target_ratio)
 
     # Copy iati-activities element so we can repeat it verbatim
-    base = deepcopy(root)
-    for base_activity in base.getchildren():
-        base.remove(base_activity)
+    base = etree.Element("iati-activities", **root.attrib)
 
     # Incrementally copy activities into blank bases
     base_copy = deepcopy(base)
     for i in range(0, len(root_activities)):
         root_activity = deepcopy(root_activities[i])
         base_copy.append(root_activity)
-        if i % max_activities == max_activities - 1:  # Write once for every max_activities
+        if i % max_activities == max_activities - 1 or i == len(root_activities) - 1:  # Write once for every max_activities or once we reach the end
             doc = etree.ElementTree(base_copy)
             file_counter = floor(i / max_activities)
-            with open("{}-{}.xml".format(filename_base, file_counter), "wb") as xmlfile:
-                doc.write(xmlfile, encoding="utf-8", pretty_print=True)
-            base_copy = deepcopy(base)
-        elif i == len(root_activities) - 1:  # Or once we reach the end
-            doc = etree.ElementTree(base_copy)
-            file_counter = floor(i / max_activities)
-            with open("{}-{}.xml".format(filename_base, file_counter), "wb") as xmlfile:
+            with open("{}{}{}-{}.xml".format(out_dir, os.path.sep, filename_base, file_counter), "wb") as xmlfile:
                 doc.write(xmlfile, encoding="utf-8", pretty_print=True)
             base_copy = deepcopy(base)
 
 
 if __name__ == "__main__":
-    max_activities = 10
-    filename = "/home/alex/git/iati-splitter/test.xml"
-    split_iati(max_activities, filename)
+    target_mb = 1
+    filename = "/home/alex/git/iati-splitter/Japan/Japan-Technical-Cooperation-Activities.xml"
+    out_dir = "/home/alex/git/iati-splitter/Japan/Japan-split"
+    split_iati(target_mb, filename, out_dir)
